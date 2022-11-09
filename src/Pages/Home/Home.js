5cd51b3427a18204485e79cd49f2d1f40bef4a14
Springhawk27 <sajjad0mahmud@gmail.com>
2022-11-09 21:31:28 +0600

    component added, router and firebase setup done

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div className='sm:w-3/4 w-3/5 mx-auto'>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 pt-8 mx-auto">
+                    <div className="text-center mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-blue-900 mb-4">Check out my available services</h1>
+                        <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">With my services you will get whatever you want no time</p>
+                        <div className="flex mt-6 justify-center">
+                            <div className="w-16 h-1 rounded-full bg-blue-900 inline-flex"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-8 mx-auto">
+                    {/* <div className="flex flex-wrap -m-4">
+                        {
+                            courses.map(course => <CourseCard
+                                key={course.id}
+                                course={course}
+                            ></CourseCard>)
+                        }
+                    </div> */}
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.png';
+
+const Header = () => {
+    return (
+        <header className="body-font bg-gray-100  sticky top-0 z-10" >
+            {/* className={`body-font bg-gray-100  sticky top-0 z-10 ${theme === true ? "bg-gray-100" : theme === false ? "bg-blue-300" : "bg-red-100"}`} */}
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <Link to="/" className="flex title-font font-medium items-center  mb-4 md:mb-0">
+
+                    <img src={logo} alt="" style={{ width: '100px' }} />
+
+                    <span className="ml-3 text-xl text-blue-900">Newsroom Psithurism</span>
+                </Link>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <Link
+
+                        to="/"
+                        className="mr-5 hover:text-blue-900">Home</Link>
+                    <Link
+
+                        to="/courses"
+                        className="mr-5 hover:text-blue-900">Courses</Link>
+                    <Link
+
+                        to="/faq"
+                        className="mr-5 hover:text-blue-900">FAQ</Link>
+                    <Link
+
+                        to="/blog"
+                        className="mr-5 hover:text-blue-900">Blog</Link>
+
+                    <div className="flex flex-wrap gap-2">
+
+                        {/* <div>
+                            <Button
+                                color="light"
+                                pill={true}
+                                onClick={() => {
+                                    setTheme(theme === false ? true : theme === true ? false : true)
+                                }}
+                            >
+                                Theme
+                            </Button>
+                        </div> */}
+                    </div>
+
+
+
+                    {/* <>
+                        {
+                            user?.uid ?
+                                <>
+                                    <span className='mr-4 sm:ml-12 ml-4'>{user?.displayName}</span>
+                                    <Button variant="light" onClick={handleLogOut}>Log out</Button>
+                                </>
+                                :
+                                <>
+                                    <Link
+                                        to='/login'
+                                        className="mr-2 "
+                                    >Login</Link>
+                                    <Link to='/register'
+                                        className="mr-2 "
+                                    >Register</Link>
+                                </>
+                        }
+                    </> */}
+                    {/* 
+                    <Tooltip
+                        content={user?.displayName ? `Hello, ${user?.displayName} ` : "Hello"}
+                        animation="duration-500"
+                        placement="left"
+                    >
+                        <Link to="/profile">
+                            {user?.photoURL ?
+                                <img
+                                    className="ml-4 h-8 w-8 rounded-full shadow-lg"
+                                    src={user?.photoURL}
+                                    alt="profile">
+                                </img>
+
+                                : <FaUser></FaUser>
+                            }
+                        </Link>
+                    </Tooltip> */}
+                </nav>
+
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
